8ff38387a8a08fc98a3791f96b44c36c2bc8fc3e
Ali Furkan Aksu <ali_furkan_aksu@hotmail.com>
2021-02-04 18:24:59 +0300

    03.02

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+//Generic Constraint
+//class : referans tip olabilir
+//IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
+//new : yenilenebilir olmalı, IEntity koyamayız
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        
+    }
+}
