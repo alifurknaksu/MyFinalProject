e382c42c192cd302400ba9fbd1ce7e9ed450e0f5
Ali Furkan Aksu <ali_furkan_aksu@hotmail.com>
2021-03-16 21:54:03 +0300

    10.03

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
@@ -28,6 +29,7 @@ namespace WebAPI.Controllers
         [HttpGet("GetAll")]
         public IActionResult GettAll()
         {
+            Thread.Sleep(1000);
             var result = _productService.GetAll();
             if (result.Success)
             {
